c040f5b1742ba2e20f7f554e3a4069f1c3a986d0
mobiitz <mobilenetzllc@gmail.com>
2026-03-28 22:49:18 -0400

    Fix asset base path for custom domain

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import path from "node:path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 export default defineConfig({
-    base: "/farm/",
+    base: "./",
     plugins: [react()],
     resolve: {
         alias: {
